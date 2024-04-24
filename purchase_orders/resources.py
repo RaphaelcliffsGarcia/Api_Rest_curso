7d861578743ba0ac6aa9f9e3c3bf21474adacde5
RaphaelcliffsGarcia <cliffsdesigner@gmail.com>
2024-04-24 18:40:52 -0300

    refatorando e adicionand banco de db

--- purchase_orders/resources.py
+++ purchase_orders/resources.py
@@ -1,24 +1,9 @@
 from flask import jsonify
 from flask_restful import Resource,reqparse
-
-purchase_orders = [{
-  'id':1,
-  'description':'Purchase Order id 1',
-  'items':[{
-    'id':1,
-    'description':'item do pedido 1',
-    'price':20.99    
-  }]                  
-}]
+from .model import PurchaseOrderModel
 
 class PurchaseOrders(Resource):
   parser = reqparse.RequestParser()
-  parser.add_argument(
-    'id',
-    type=int,
-    required=True,
-    help="Informe um Id Valido"
-  )
   parser.add_argument(
     'description',
     type=str,
@@ -26,21 +11,18 @@ class PurchaseOrders(Resource):
     help='Informe uma Descrição Valida'
   )
   def get(self):
-     return jsonify(purchase_orders)
+    purchase_orders = PurchaseOrderModel.find_all()
+    return [p.as_dict()for p in purchase_orders]
+  
   def post (self):
     data = PurchaseOrders.parser.parse_args()
-    purchase_order= {
-      'id': data['id'],
-       'description':data['description'],
-       'items':[]
-    }
-     
-    purchase_orders.append(purchase_order)
-    return jsonify(purchase_order)
+    purchase_order = PurchaseOrderModel(**data)
+    purchase_order.save()
+    return purchase_order.as_dict()
 
 class PurchaseOrderById(Resource):
-  def get (self,id):
-    for po in purchase_orders:
-      if po ['id'] == id:
-        return jsonify(po)
-    return jsonify({'message':'Pedido de id {} não encontrado'.format(id)})
+    def get(self, id):
+        purchase_order = PurchaseOrderModel.find_by_id(id)
+        if purchase_order:
+            return purchase_order.as_dict()
+        return jsonify({'message': 'Pedido de id {} não encontrado'.format(id)})
