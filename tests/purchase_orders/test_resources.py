ece87bfda114b2f0dd2d5b6ee30254f6cf382d69
RaphaelcliffsGarcia <cliffsdesigner@gmail.com>
2024-04-26 19:51:12 -0300

    refatorando

--- tests/purchase_orders/test_resources.py
+++ tests/purchase_orders/test_resources.py
@@ -1,50 +1,39 @@
 import json
 
-def test_get_purchase_orders(test_client):
+def test_get_purchase_orders(test_client,seed_db):
   response = test_client.get('/purchase_orders')
   assert response.status_code == 200
-  assert response.json[0]["id"]== 1
-  assert response.json[0]['description'] == 'Purchase Order id 1'
-  assert len(response.json[0]['items']) == 1
-  assert response.json[0]['items'][0]['id']==1
+  assert response.json[0]["id"]== seed_db.id
+  assert response.json[0]['description'] == seed_db.description
   
 def test_post_purchase_orders(test_client):
-  obj = { 'id':2,'description': 'Purchase Order id 2'}
+  obj = {'description': 'Purchase Order id 2'}
   response = test_client.post(
     '/purchase_orders',
     data = json.dumps(obj),
     content_type='application/json'
     )
   assert response.status_code== 200
-  assert response.json['id']== obj['id']
+  assert response.json['id'] is not None
   assert response.json['description'] == obj['description']
-  assert response.json['items']==[]
   
-def test_post_empty_id(test_client):
-  response = test_client.post(
-    '/purchase_orders',
-    data = json.dumps({'description':'Descrição'}),
-    content_type='application/json'
-  )
-  assert response.status_code == 400
-  assert response.json['message']['id'] == 'Informe um Id Valido'
   
 def test_post_empty_description(test_client):
   response = test_client.post(
     '/purchase_orders',
-    data = json.dumps({'id':2}),
+    data = json.dumps({}),
     content_type='application/json'
   )
    
   assert response.status_code == 400
   assert response.json['message']['description'] == 'Informe uma Descrição Valida'
 
-def test_get_purchase_order_by_id(test_client):
-  response = test_client.get('/purchase_orders/1')
+def test_get_purchase_order_by_id(test_client,seed_db):
+  response = test_client.get('/purchase_orders/{}'.format(seed_db.id))
   
   assert response.status_code == 200
-  assert response.json['id'] == 1
-  assert response.json['description'] =='Purchase Order id 1'
+  assert response.json['id'] == seed_db.id
+  assert response.json['description'] == seed_db.description
 
 def test_get_purchase_order_not_found(test_client):
   id = 9999
